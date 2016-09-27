8690ac66e8e9bf455d8e45707e8bc7dbd8f44c76
mehrdadjaliliyan <mehrdad.jaliliyan@gmail.com>
2016-09-27 15:54:34 +0330

    تغییر تستی

--- app/src/main/java/com/test/mehrdad/testanim/MainActivity.java
+++ app/src/main/java/com/test/mehrdad/testanim/MainActivity.java
@@ -4,10 +4,20 @@ package com.test.mehrdad.testanim;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.test.mehrdad.testanim.chart.ChartFragment;
+import com.test.mehrdad.testanim.menutest.Main2Activity;
+import com.test.mehrdad.testanim.rsa.RSA;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button =(Button) findViewById(R.id.buttonshowChart);
+
+
+        RSA rsa =  new RSA();
+        try {
+            String strEncrypt = rsa.Encrypt("test");
+            Log.d("RSA" , "Encrypt = " + strEncrypt);
+            String strDecrypt = rsa.Decrypt(strEncrypt);
+            Log.d("RSA" , "Decrypt = " + strDecrypt);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         Button button2 =(Button) findViewById(R.id.buttonMp);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +66,10 @@ public class MainActivity extends AppCompatActivity {
 
 //                Intent intent = new Intent(MainActivity.this,MpChartActivity.class);
 
-                Intent intent = new Intent(MainActivity.this,WebViewActivity.class);
+//                Intent intent = new Intent(MainActivity.this,WebViewActivity.class);
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+
 
                 // Start Activity
                 startActivity(intent);
